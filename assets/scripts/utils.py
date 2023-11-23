d27cdd0574c78a7c4fce710fecd98b7c168b081e
shiro1309 <Nicolay5lie@gmail.com>
2023-11-23 22:44:52 +0100

    animation and sprite sheet loaders is now working but curently only for the entity animation and not for tilemaps
     Changes to be committed:
    	modified:   assets/scripts/__pycache__/utils.cpython-311.pyc
    	modified:   assets/scripts/utils.py
    	modified:   assets/sprites/Sprite-0001.png
    	modified:   testfileloader.py

--- assets/scripts/utils.py
+++ assets/scripts/utils.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 def draw_text(text, x, y, color, font, surf):
         text_surface = font.render(text, True, color)
@@ -17,11 +18,50 @@ def load_sprites(sprite_sheet_path, sprite_size, animation_names):
     for i, name in enumerate(animation_names):
         sprites = []
         for x in range(sprite_sheet.get_width() // sprite_size[0]):
+            print(x * sprite_size[0], i * sprite_size[1], sprite_size[0], sprite_size[1])
             sprite = pygame.Surface.subsurface(sprite_sheet, (x * sprite_size[0], i * sprite_size[1], sprite_size[0], sprite_size[1]))
             sprites.append(sprite)
         
         sprite_sheets[name] = sprites
     return sprite_sheets
+
+class Animation:
+    def __init__(self, sprite_dict, image_duration=1, loop=True):
+        self.sprite_dict = sprite_dict
+        self.image_duration = 1 / image_duration
+        self.loop = loop
+        self.current_animation = list(sprite_dict.keys())[0]  # Set the default animation to the first one
+        self.frame = 0
+        self.done = False
+        self.animation_sum = 0
+        self.animation_start = time.time()
+
+    def update(self):
+        #print(time.time() - self.animation_start, self.animation_sum + self.image_duration)
+        if time.time() - self.animation_start >= self.animation_sum + self.image_duration:
+            self.animation_sum += self.image_duration
+            self.animate()
+
+    def set_animation(self, animation_name):
+        if animation_name in self.sprite_dict:
+            self.current_animation = animation_name
+            self.frame = 0
+            self.done = False
+            self.animation_start = time.time()
+
+    def copy(self):
+        return Animation(self.sprite_dict, self.image_duration, self.loop)
+
+    def animate(self):
+        if self.loop:
+            self.frame = (self.frame + 1) % (len(self.sprite_dict[self.current_animation]))
+        else:
+            self.frame = min(self.frame + 1, len(self.sprite_dict[self.current_animation]) - 1)
+            if self.frame == len(self.sprite_dict[self.current_animation]) - 1:
+                self.done = True
+
+    def img(self):
+        return self.sprite_dict[self.current_animation][self.frame]
 
 class Button:
     def __init__(self, text, rect, action, color):
@@ -30,3 +70,4 @@ class Button:
         self.action = action
         self.color = color
         self.font = pygame.font.Font(None, 36)
+
--- testfileloader.py
+++ testfileloader.py
@@ -1,19 +1,46 @@
-# main.py
 import pygame
-from assets.scripts.utils import load_sprites
+import sys
+from assets.scripts.utils import load_sprites, Animation  # Assuming you have your spritesheet loader function
+
+# Initialize pygame
 pygame.init()
-screen = pygame.display.set_mode((200,200))
 
-# Load sprites from a sprite sheet with individual sprite size (32x32)
-# For this example, assume "walk" and "jump" are the animation names
-player_animations = load_sprites("assets/Sprite-0001.png", (16, 16), animation_names=["walk", "jump"])
-print(player_animations)
-# Access sprites for the "walk" animation
-walk_animation_sprites = player_animations["walk"]
+# Set up window
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Animation Example")
+clock = pygame.time.Clock()
 
-# Access sprites for the "jump" animation
-jump_animation_sprites = player_animations["jump"]
+# Load player sprites
+player_sprites = load_sprites("assets\sprites\Sprite-0001.png", (16,16), ["walk", "jump"])
+player_animation = Animation(player_sprites, image_duration=24, loop=True)
 
-print(walk_animation_sprites, jump_animation_sprites)
+# Set initial position of player
+player_x, player_y = WIDTH // 2, HEIGHT // 2
+clock = pygame.time.Clock()
 
-# Use the sprites in your game
+# Main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Update the animation
+    player_animation.update()
+    clock.tick(60)
+
+    # Clear the screen
+    screen.fill((125,125,125))
+
+    # Get the current frame of the animation
+    current_frame = player_animation.img()
+
+    # Draw the current frame to the screen at the player's position
+    screen.blit(current_frame, (player_x, player_y))
+
+    # Update the display
+    pygame.display.flip()
+
+    # Cap the frame rate
+    clock.tick(60)
